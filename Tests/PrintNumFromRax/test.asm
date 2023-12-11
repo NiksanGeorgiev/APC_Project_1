7023c92e5aa9a4f269d27a50a6f95a050cc14259
NiksanGeorgiev <66817169+NiksanGeorgiev@users.noreply.github.com>
2023-12-11 11:28:52 +0100

    All tests
    Mainly in PrintNumFromRax

--- Tests/PrintNumFromRax/test.asm
+++ Tests/PrintNumFromRax/test.asm
@@ -1,16 +1,76 @@
 ; source: https://www.youtube.com/watch?v=XuUD0WQ9kaE
+; syscalls
+SYS_READ    equ 0
+SYS_WRITE   equ 1
+SYS_OPEN    equ 2
+SYS_CLOSE   equ 3
+SYS_EXIT    equ 60
+
+; File Descriptors
+STDIN   equ 0
+STDOUT  equ 1
+
+; Open modes
+O_RDONLY    equ 0
+O_WRONLY    equ 1
+O_RDWR      equ 2
+
+NEW_LINE    equ 10              ; '\n'
+CAR_RET     equ 13              ; '\r'
+ASCII0      equ 48              ; 0 in ASCII
 section .bss
     digitSpace resb 100 
     digitSpacePos resb 8 ; enough to store a value of a register
+    num1 resb 8
+    num2 resb 8
+    num3 resb 8
 
+    line resb 60
+    buffer resb 2400
+
+section .data
+    number dw 0
+    num db 10, "12345",0
+    ipath db "input.txt",0
+    path db "output.txt",0
+    text db "Some text",0
+    
 section .text
     global _start
  
 _start:
+    ; mov rbx, 6
+    ; mov [num1], rbx
+    ; mov rbx, 70
+    ; mov [num2], rbx
+    ; mov rbx, 1
+    ; mov [num3], rbx
+
+    ; ; ; mov rbx, rax
+    ; ; ; mov rax, [rbx]
+
+    ; ; mov rbx, 69420
+    ; ; mov [num1], rbx  ; stores value into a variable
+    ; ; mov rax, num1    ; sores the address of the variable
+    ; ; ; mov rbx, [rax]      ; practically dereference the address of the variable
+    ; ; ; mov rax, rbx
+    ; ; mov rbx, 10
+    ; ; mov [rax], rbx
+    ; ; mov rax, [num1]   
+    ; ;call _sum_top_three
+
+    ; mov rax, [number]
+    ; call _printRAX
  
-    mov rax, 69420 ; move to rax the number to be printed
-    call _printRAX
- 
+    ; mov rbx, 2
+    ; mov [number], rbx
+    mov rdx, 0
+    mov [number], rdx
+    ; call _read_line
+    call _read_line
+    ;call _printRAX
+    call _write_to_file
+
     mov rax, 60
     mov rdi, 0 
     syscall
@@ -61,3 +121,189 @@ _printRAXLoop2:
     jge _printRAXLoop2
  
     ret
+
+_min:
+    ; Move numbers into registers so that they can be compared
+    mov rbx, [num1]
+    mov rcx, [num2]
+    mov rdx, [num3]
+    xor rax, rax                ; Reset rax
+
+    cmp rbx, rcx
+    jle rbx_min
+    jmp rcx_min
+
+rbx_min:
+    cmp rbx, rdx
+    jle rbx_store
+    jmp rdx_store
+
+rcx_min:
+    cmp rcx, rdx
+    jle rcx_store
+    jmp rdx_store
+rbx_store:
+    mov rax, num1
+    jmp return
+rcx_store:
+    mov rax, num2
+    jmp return
+rdx_store:
+    mov rax, num3
+    jmp return
+
+return:
+    ret
+
+_sum_top_three:
+xor rax, rax
+mov rbx, [num1]
+add rax, rbx
+mov rbx, [num2]
+add rax, rbx
+mov rbx, [num3]
+add rax, rbx
+ret
+
+_read_line:
+        
+    ;read a line
+    mov rax, 0               ; Mode for reading
+    mov rdi, 0               ; File descriptor of the console
+    mov rsi, line            ; Buffer to store the input   
+    mov rdx, 60              ; Number of bytes to be read
+    syscall
+
+    call _validate_line
+    cmp rax, 0
+    je _read_line
+;count the length of the line (\n inclusive) 
+    xor rax,rax
+    mov rdi, line
+    mov rbx, buffer
+    mov rcx, [number]
+    
+add_char:
+    movzx rsi, byte [rdi]
+    
+    cmp rsi, 'n'
+    je retur
+
+    cmp rsi, 0
+    je end_line
+    mov [buffer + rcx + rax], rsi
+    inc rax
+    inc rdi
+    cmp rsi , 10
+    je end_line
+    jmp add_char
+
+end_line:
+    add rcx, rax
+    mov [number], rcx
+    jmp _read_line
+
+
+; add_buffer:
+;     mov rdi, line
+;     mov rdx, [number]
+;     add rdx, rax
+;     mov [number], rdx
+
+; add_char:
+;     movzx rsi, byte [rdi]
+;     cmp rsi, 0
+;     je _read_line
+;     mov [buffer + rdx], rsi
+;     dec rdx
+;     inc rdi
+;     jmp add_char
+
+retur:
+
+    mov rax, [number]
+    mov rbx, 0
+    mov [buffer + rax], rbx
+    ret
+    
+
+; _fill_buffer:
+
+;     mov rdi, text
+;     xor rax,rax
+; char:
+;     movzx rsi, byte [rdi]
+;     cmp rsi, 0
+;     je r
+;     inc rdi
+;     mov [buffer + rax], rsi
+;     inc rax
+;     jmp char
+
+; r:
+;     ret
+
+_write_to_file:
+
+    mov rax, 2
+    mov rdi, path
+    mov rsi, 64+1
+    mov rdx, 0644o
+    syscall
+
+    push rax
+    mov rdi, rax
+    mov rax, 1
+    mov rsi, buffer
+    mov rdx, [number]
+    syscall
+
+    mov rax, 3
+    pop rdi
+    syscall
+
+    ret
+
+_validate_line:
+    mov rdi, line               ; Load the address of the line to check in rdi
+
+validation_loop:
+    movzx rsi, byte [rdi]       ; Get current character
+    
+    cmp rsi, 0                  ; End of line - successful
+    je valid
+
+    cmp rsi, 10
+    je valid
+
+    cmp rsi, 'n'
+    je valid
+
+    cmp rsi, ASCII0             ; Smaller than the ascii value of 0
+    jl invalid
+    
+    cmp rsi, ASCII0 + 9         ; Bigger than the ascii value of 9
+    jg invalid
+
+    inc rdi                     ; Get to the next character
+    jmp validation_loop
+
+invalid:
+    xor rax, rax
+    jmp end_validation
+valid:
+    mov rax, 1
+end_validation:
+    ret 
+
+_print_backwards:
+    mov rax, 6 ; starting position
+    
+
+
+    mov rax, SYS_WRITE          ; Type of operation (syscall number) 
+    mov rdi, STDOUT             ; Where to write to (file descriptor)
+    mov rsi, rcx                ; The address of the buffer to be printed
+    mov rdx, 1                  ; Number of bytes to be printed
+    syscall
+    mov
--- Tests/ProjectMock/test.asm
+++ Tests/ProjectMock/test.asm
@@ -23,13 +23,32 @@ ASCII0      equ 48              ; 0 in ASCII
 section .data
     path dd "input.txt"         ; Path to the file containing the input
     size dw 24000               ; Size of the bugffer that is going to be used for storing the file
+    l_count dd 0                ; Will store the number of characters during manual input       
+    ; Console output
+    introduction db "Elves' expedition traditionally goes on foot.", NEW_LINE, "As your boats approach land, the Elves begin taking inventory of their supplies.", NEW_LINE, "One important consideration is food - in particular, the number of Calories each Elf is carrying",NEW_LINE, NEW_LINE,0
+    introduction1 db "The Elves take turns writing down the number of Calories contained by the various meals",NEW_LINE, "that they've brought with them, one item per line.", NEW_LINE, "Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.",NEW_LINE, NEW_LINE,0
+    part1 db "PART 1:", NEW_LINE, "In case the Elves get hungry and need extra snacks, they need to know which Elf to ask.",NEW_LINE, "They'd like to know how many Calories are being carried by the Elf carrying the most Calories.", NEW_LINE, NEW_LINE, 0
+    part2 db "PART 2:", NEW_LINE, "To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried", NEW_LINE, "by the top three Elves carrying the most Calories.", NEW_LINE, "That way, even if one of those Elves runs out of snacks, they still have two backups.", NEW_LINE, NEW_LINE, 0
+    example_input db "Example input:", NEW_LINE, "1000",NEW_LINE,"2000", NEW_LINE, "3000", NEW_LINE, NEW_LINE, "4000", NEW_LINE, NEW_LINE, "5000", NEW_LINE, "6000", NEW_LINE, NEW_LINE, "7000", NEW_LINE, "8000", NEW_LINE, "9000",NEW_LINE,NEW_LINE, "Highest - 24000",NEW_LINE, "Sum of 3 highest - 45000", NEW_LINE, NEW_LINE, 0
+    highest_answer db "The highest calorie count is:", NEW_LINE, 0
+    top_three_answer db "The sum of the 3 highest calorie counts is:", NEW_LINE, 0
+    manual_input_prompt db "Choose (m)anual or (f)ile input", NEW_LINE, "Invalid input will be ignored", NEW_LINE, 0
+    manual_rules db "Input meals on different lines. Separate elfs with an empty line.", NEW_LINE, "(Lines not containing only integers will be ignored e.g. 123as1)", NEW_LINE, "Begin a line with 'n' to stop inputing", NEW_LINE, 0
+    cls db `\033[H\033[2J`, 0   ; ANSI escape code for "clearing" console
 
 ;-----------------MEMORY RESERVATIONS-----------------
 section .bss
+    manual: resb 1              ; Will store whether the input will be manual
     buffer: resb 24000          ; Buffer big enough to store the input file
     digitString: resb 100       ; Stores the string representation of a number
     digitIndex: resb 8          ; Enough to store a value of a register
-
+    lineNum: resb 8             ; Will be storing the integer value of the number on the line
+    blockSum: resb 8            ; Will be storing the block sum
+    highest: resb 8             ; Will be storing the highest num sum
+    num1: resb 8                ; Will be used to store one of the three highest numbers
+    num2: resb 8                ; Will be used to store one of the three highest numbers
+    num3: resb 8                ; Will be used to store one of the three highest numbers
+    line: resb 40               ; Will store the line during manual input
 ;-----------------MACROS-----------------
 ; !DISCLAIMER! NASM specific syntax
 ; @param - number to be printed
@@ -44,38 +63,105 @@ section .bss
     syscall
 %endmacro
 
+; @param - the address of the string
+%macro print_string 1
+    mov rax, %1
+    call _print_string
+%endmacro
+
+
+    
+; %endmacro
 ;-----------------PROGRAM-----------------
 section .text
     global _start               
 
 _start:
+    call _print_intro           ; Prints the introduction to the program
+    call _manual_input          ; Returns whether the user wants manual or file input
 
-    call _read_file
+    mov rax, [manual]
+    cmp rax, 'm'                ; Check what type of input was chosen
+    je manual_input
+    jmp file_input
 
-    ;Register initialisation
-    mov rdi, buffer             ; Load address of the input string
-    xor rax, rax                ; Will be used for storing the number read on a line
-    xor rbx, rbx                ; Will be used for storing the sum of block of numbers
-    xor rcx, rcx                ; Will be used for keeping track of the highest sum
+manual_input:
+    print_string cls            ; Clear console
+    xor rax, rax
+    mov [l_count], rax          ; Will be used for letter count of manual input  
+    print_string manual_rules   ; Indicating how to use the manual input
+    call _read_console          ; Reads input from the console
+    jmp main_logic
+
+file_input:
+    call _read_file             ; Reads input from a file
+
+main_logic:
+    ; Register initialisation
+    xor rax, rax                
+    mov [lineNum], rax          ; Will be used for storing the number read on a line
+    mov [blockSum], rax         ; Will be used for storing the sum of block of numbers
+    mov [highest], rax          ; Will be used for keeping track of the highest sum
     xor r8, r8                  ; Will be used for representing the length of a number
     
+    ; Find and print highest sum
+    print_string highest_answer
+    mov rdi, buffer             ; Load address of the input string
     call _find_highest
+    print_number rax            ; Print the highest value
 
-    print_number rax                   ; Print the highest value
+    ; Find and print the sum of the top 3
+    print_string top_three_answer
+    call _sum_top_three
+    print_number rax            ; Print the sum (rax is holding the value)
     exit
 
 ;-------------------------------------------
 ;-----------------FUNCTIONS-----------------
 ;-------------------------------------------
+
+;-----------------START - PRINT INTRO-----------------
+_print_intro:
+    print_string cls
+    print_string introduction
+    print_string introduction1
+    print_string part1
+    print_string part2
+    print_string example_input
+    print_string manual_input_prompt
+ret
+;-----------------END - PRINT INTRO-----------------
+
+;-----------------START - INPUT CHOICE-----------------
+_manual_input:
+validate:
+    
+    mov rax, SYS_READ           ; Mode for reading
+    mov rdi, STDIN              ; File descriptor of the console
+    mov rsi, manual             ; Buffer to store the input   
+    mov rdx, 1                  ; Number of bytes to be read
+    syscall 
+
+    mov rdx, [manual]           ; Store the input in rdx
+    cmp rdx, 'm'                ; 155 - 'm' in ascii
+    je return_input 
+    cmp rdx, 'f'                ; 146 - 'f' in ascii
+    jne validate
+
+return_input:
+    ret
+;-----------------END - INPUT CHOICE-----------------
+
+;-----------------START - READ FILE-----------------
 _read_file:
-    ;Opening the file to get the file descriptor
+    ; Opening the file to get the file descriptor
     mov rax, SYS_OPEN           ; Type of operation (syscall number) 
     mov rdi, path               ; Path to file
     mov rsi, O_RDONLY           ; Mode of opening
     mov rdx, 0644o              ; file permission which doesn't really matter when reading a file
     syscall
 
-    ;Reading the file and storing it in a buffer
+    ; Reading the file and storing it in a buffer
     mov rdi, rax                ; File descriptor (returned automatically in rax)
     mov rax, SYS_READ           ; Type of operation (syscall number) 
     mov rsi, buffer             ; Buffer to store the file
@@ -83,6 +169,28 @@ _read_file:
     syscall
 
     ret
+;-----------------END - READ FILE-----------------
+
+;-----------------START - PRINT STRING-----------------
+; Input: rax as pointer to string
+_print_string:
+    push rax
+    mov rbx, 0
+print_string_loop:
+    inc rax
+    inc rbx
+    mov cl, [rax]
+    cmp cl, 0
+    jne print_string_loop
+ 
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    pop rsi
+    mov rdx, rbx
+    syscall
+ 
+    ret
+;-----------------END - PRINT STRING-----------------
 
 ;-----------------START - PRINT NUMBER-----------------
 _print_num:
@@ -136,6 +244,7 @@ print_num_loop:
 ;-----------------START - FIND MAXIMUM-----------------
 _find_highest:
     
+    mov rax, [lineNum]          ; Load the value of the line into rax
     movzx rsi, byte [rdi]       ; Read a character from the input
     
     cmp rsi, 0                  ; Check if the character is the null terminator (end of string)
@@ -154,7 +263,7 @@ _find_highest:
 multiply:
     mov r9, 10                  ; Multiplier
     mul r9                      ; Multiplying the value of rax by 10
-
+    
 addition:
     sub rsi, ASCII0             ; Convert ASCII character to integer
     add rax, rsi                ; Add to the sum
@@ -163,48 +272,234 @@ increment:
     inc rdi                     ; Move to the next character in the input string
     inc r8                      ; Increment the number of digits in the number on the current line
     
-    jmp _find_highest            ; Continue reading characters
+    mov [lineNum], rax          ; Update the value of the line number
+    jmp _find_highest           ; Continue reading characters
 
 check_empty_line:
     ; Actions done at the end of a line
     xor r8, r8                  ; Reset the number of digits
+    mov rbx, [blockSum]         ; Load the block sum into rbx
+    mov rax, [lineNum]          ; Load the line number into rax
     add rbx, rax                ; Add the value of the line to the sum of the block
     xor rax, rax                ; Reseting the line sum
+    mov [blockSum], rbx         ; Updating the block sum
+    mov [lineNum], rax          ; Updating the line number
 
-
-    movzx rsi, byte [rdi - 2]   ; Get the value value 2 characters behind the current
+    ; Mac file formating (without \r)
+    movzx rsi, byte [rdi - 1]   ; Get the value value 1 character behind the current
     cmp rsi, NEW_LINE           ; Check if it is \n    
     je update_block_sum         ; If yes, update block sum and reset block sum
     
-    inc rdi                     ; Point to next character
-    jmp _find_highest            ; Continue reading characters
+    ; Windows line formatting
+    cmp rsi, CAR_RET
+    je check_windows_line
 
+check_windows_line:
+    movzx rsi, byte [rdi - 2]   ; Get the value value 2 characters behind the current
+    cmp rsi, NEW_LINE           ; Check if it is \n    
+    je update_block_sum         ; If yes, update block sum and reset block sum
+
+    inc rdi                     ; Point to next character
+    jmp _find_highest           ; Continue reading characters
 
 update_block_sum:
+    call _min                   ; Get address of the samllest out of the three highest numbers
+    mov rbx, [rax]              ; Store the value of the number
+    mov rcx, [blockSum]         ; Get the block sum so it can be compared
+    cmp rbx, rcx                ; If the block sum is greater - update the 3 highest
+    jl update_three_highest
+    jmp post_three_update       ; Skip update if not needed
+
+update_three_highest:
+    mov [rax], rcx              ; Move the block sum into the address
+    xor rax, rax                ; Reset rax
+
+post_three_update:
+    mov rbx, [blockSum]         ; Load the block sum into rbx
+    mov rcx, [highest]          ; Load the highest num into rcx
     cmp rbx, rcx                ; Compare and update maximum sum
     jg update_maximum_sum
 
     xor rbx, rbx                ; Reset block sum for the next block
+    mov [blockSum], rbx         ; Update block sum
     inc rdi                     ; Move to the next character in the input string
-    jmp _find_highest            ; Continue reading characters
+    jmp _find_highest           ; Continue reading characters
 
 update_maximum_sum:
+    mov rbx, [blockSum]         ; Load the block sum into rbx
+    mov rcx, [highest]          ; Load the highest num into rcx
     mov rcx, rbx                ; Update block sum
     xor rbx, rbx                ; Reset block sum for the next block
+    mov [blockSum], rbx         ; Update vlock sum
+    mov [highest], rcx          ; Update highest num
     inc rdi                     ; Move to the next character in the input string
-    jmp _find_highest            ; Continue reading characters
+    jmp _find_highest           ; Continue reading characters
 
 end_reading:
+    mov rbx, [blockSum]         ; Get block sum
+    add rbx, rax                ; Account for the last number
+    mov [blockSum], rbx         ; Update block sum
+    call _min                   ; Get address of the samllest out of the three highest numbers
+    mov rbx, [rax]              ; Store the value of the number
+    mov rcx, [blockSum]         ; Get the block sum so it can be compared
+    cmp rbx, rcx                ; If the block sum is greater - update the 3 highest
+    jl update_three
+    jmp final_check             ; Skip update if not needed
+
+update_three:
+    mov [rax], rcx              ; Move the block sum into the address
+    xor rax, rax                ; Reset rax
+
+final_check:
+    mov rbx, [blockSum]         ; Load the block sum into rbx
+    mov rcx, [highest]          ; Load the highest num into rcx
     cmp rbx, rcx                ; Last comparisson
     jg swap                     ; If last block sum is bigger then swap values
     jmp end                     ; Else go to the end
 
 swap:
+    mov rbx, [blockSum]         ; Load the block sum into rbx
+    mov rcx, [highest]          ; Load the highest num into rcx
     mov rcx, rbx                ; Update maximum value     
-    xor rbx, rbx                ; Reset rbx
+    mov [highest], rcx          ; Update highest num
 
 end:
-    mov rax, rcx                ; Common convention for functions to return result in rax
+    mov rax, [highest]          ; Common convention for functions to return result in rax
     ret
 
 ;-----------------END - FIND MAXIMUM-----------------
+
+;-----------------START - FIND MINIMUM-----------------
+; Find the minimum value out of num1, num2 and num3
+; @return - the address of the corresponding variable
+_min:
+    ; Move numbers into registers so that they can be compared
+    mov rbx, [num1]
+    mov rcx, [num2]
+    mov rdx, [num3]
+    xor rax, rax                ; Reset rax
+
+    cmp rbx, rcx                ; Check if num1 is smaller than num2
+    jle rbx_min                 
+    jmp rcx_min
+
+rbx_min:
+    cmp rbx, rdx                ; Check if num1 is smaller than num3
+    jle rbx_store
+    jmp rdx_store
+
+rcx_min:
+    cmp rcx, rdx                ; Check if num2 is smaller than num3
+    jle rcx_store
+    jmp rdx_store
+
+rbx_store:
+    mov rax, num1               ; num1 is the min out of the 3
+    jmp return
+rcx_store:
+    mov rax, num2               ; num2 is the min out of the 3
+    jmp return
+rdx_store:
+    mov rax, num3               ; num3 is the min out of the 3
+    jmp return
+
+return:
+    ret
+;-----------------END - FIND MINIMUM-----------------
+
+_sum_top_three:
+xor rax, rax
+mov rbx, [num1]
+add rax, rbx
+mov rbx, [num2]
+add rax, rbx
+mov rbx, [num3]
+add rax, rbx                    ; Storing the result in rax to meet common conventions
+ret
+
+;-----------------START - MANUAL INPUT-----------------
+_read_console:
+        
+    ;read a line
+    mov rax, SYS_READ           ; Mode for reading
+    mov rdi, STDIN              ; File descriptor of the console
+    mov rsi, line               ; Buffer to store the input   
+    mov rdx, 60                 ; Number of bytes to be read
+    syscall
+
+    ; Check if the line is valid
+    call _validate_line         
+    cmp rax, 0                  ; Invalid
+    je _read_console            ; Ignore the line and input another one
+
+    ; Count the length of the line (\n inclusive)
+    xor rax, rax                ; Reset rax
+    mov rdi, line               ; Load the address of the line in rdi   
+    mov rbx, buffer             ; Load the address of the buffer in rbx
+    mov rcx, [l_count]          ; Store the value of the letter count in rcx
+    
+add_char:
+    movzx rsi, byte [rdi]       ; Store character from the line
+    
+    cmp rsi, 'n'                ; 'n' indicates ends of input
+    je end_buffer                    
+
+    cmp rsi, 0                  ; Indicates the end of the line
+    je end_line
+    mov [buffer + rcx + rax], rsi ; Store the character in the buffer
+    inc rax                     ; Incrementing rax (incrementing the count of characters in the line)
+    inc rdi                     ; Incrementing rdi (moving to the next character in the line)
+    cmp rsi, NEW_LINE           ; Check if it is a new line    
+    je end_line
+    jmp add_char   
+    
+
+end_line:
+    add rcx, rax                ; Adding the character count of the line to the total length of the buffer
+    mov [l_count], rcx          ; Updating the character count
+    jmp _read_console  
+
+end_buffer:
+    ; Adding terminating character at the end of buffer
+    mov rax, [l_count]          
+    mov rbx, 0
+    mov [buffer + rax], rbx
+    ret
+
+;-----------------END - MANUAL INPUT-----------------
+
+;-----------------START - VALIDATE LINE-----------------
+; Validates whether the line consists of only integers
+; @return - 0 if invalid and 1 if valid
+_validate_line:
+    mov rdi, line               ; Load the address of the line to check in rdi
+
+validation_loop:
+    movzx rsi, byte [rdi]       ; Get current character
+    
+    cmp rsi, 0                  ; End of line - successful
+    je valid
+
+    cmp rsi, NEW_LINE           ; Checks for a new line
+    je valid
+
+    cmp rsi, 'n'                ; Checks for 'n' which signals the end of input
+    je valid
+
+    cmp rsi, ASCII0             ; Smaller than the ascii value of 0
+    jl invalid
+    
+    cmp rsi, ASCII0 + 9         ; Bigger than the ascii value of 9
+    jg invalid
+
+    inc rdi                     ; Get to the next character
+    jmp validation_loop
+
+invalid:
+    xor rax, rax
+    jmp end_validation
+valid:
+    mov rax, 1
+end_validation:
+    ret 
+;-----------------END - VALIDATE LINE-----------------
